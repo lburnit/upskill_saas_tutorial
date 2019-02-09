48d89b0a40a5cd716311cd9907ac71f1a8373cba
Leon Ward <leonwardbusiness@gmail.com>
2019-02-09 14:07:01 +0000

    added server side code for creating subscriptions for pro users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -60,7 +60,7 @@ $(document).on('turbolinks:load', function(){
     var token = response.id;
     
     //Inject card token in hidden field
-    theForm.append( $('<input type="hidden" name="user[stripe_card_token]>').val(token));
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token));
     
     //Submit form to our Rails app
     theForm.get(0).submit();
