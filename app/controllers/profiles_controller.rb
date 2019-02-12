29531d6088d2b8c9263e140d35e2fe84a5223756
Leon Ward <leonwardbusiness@gmail.com>
2019-02-12 09:12:16 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
   #get request to //users/:user_id/profile/new
   def new
     #render blank profile details form
+    @profile = Profile.new
   end   
 end     
